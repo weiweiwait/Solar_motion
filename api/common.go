2f7558e3b5d5517c2c3573a7e6a7c3c54ccd7226
fansg666 <2680315178@qq.com>
2024-05-09 17:34:13 +0800

    基本完成consts层

---
+++ api/common.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"Solar_motion/config"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+func ErrorResponse(ctx *gin.Context, err error) *ctl.TrackedErrorResponse {
+	if ve, ok := err.(validator.ValidationErrors); ok {
+		for _, fieldError := range ve {
+			field := config.T(fmt.Sprintf("Field.%s", fieldError.Field))
+			tag := config.T(fmt.Sprintf("Tag.Valid.%s", fieldError.Tag))
+			return ctl.RespError(ctx, err, fmt.Sprintf("%s%s", field, tag))
+		}
+	}
+
+	if _, ok := err.(*json.UnmarshalTypeError); ok {
+		return ctl.RespError(ctx, err, "JSON类型不匹配")
+	}
+
+	return ctl.RespError(ctx, err, err.Error(), e.ERROR)
+}
--- api/user.go
+++ api/user.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"Solar_motion/pkg/utils/log"
 	"Solar_motion/types"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 //用户注册
@@ -11,7 +13,8 @@ func RegisterHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var req types.UserRegisterReq
 		if err := ctx.ShouldBind(&req); err != nil {
-
+			log.LogrusObj.Infoln(err)
+			ctx.JSON(http.StatusOK)
 		}
 	}
 }
